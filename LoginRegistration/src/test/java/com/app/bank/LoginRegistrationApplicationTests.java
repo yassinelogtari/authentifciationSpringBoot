2eb1ddf34e1b9451f50c4194a4ff98c5a433085a
logtari <yassinelogtari@isamm.u-manouba.tn>
2024-02-05 10:52:44 +0100

    add project

---
+++ LoginRegistration/src/test/java/com/app/bank/LoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
